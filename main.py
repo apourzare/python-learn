79cdc4f34a6d6e2d1c70a695b57fb372e0188c97
Amin Pourzare <apourzare@gmail.com>
2021-02-20 19:45:08 +0330

    Sesstion 001, Data types

---
+++ main.py
@@ -0,0 +1,54 @@
+a = 1
+# print(a)
+# print(type(a))
+# print(a, 'is', type(a))
+
+# name = "Amin"
+# print(name, type(name))
+
+# is_docter = False
+# print(is_docter, type(is_docter))
+
+# pi = 3.14
+# print(pi, type(pi))
+
+# info = ['Amin', 'Pourzare', 'Male', 31, ['Developer', 'Designer', 'Teacher']]
+# print(info, type(info))
+# print(info[0], type(info[0]))
+# print(info[-1])
+# info[-1] = 30
+# print(info[-1][1])
+
+# info = ('Amin', 'Pourzare', 'Male', 31, ['Developer', 'Designer'])
+# print(info, type(info))
+# print(info[1])
+# info[-1] = 30 #Error
+# info[-1] = ('Teacher', 'Designer')
+# info[-1].append('Manager')
+# print(info)
+
+# a = ('Teacher',)
+# print(a, type(a))
+
+# info = {'fname': 'Amin', 'lname': 'Pourzare', 'gender': 'Male', 'age': 31}
+# print(info , type(info))
+# print(info['fname'])
+# info['age'] = 30
+# info['age'] = 30
+# print(info)
+
+# info = {'Amin', 'Pourzare', 'Male', 31, 'Amin'}
+# print(info, type(info))
+
+a, b = 1, 5
+print('a is', a)
+print("b is", b)
+print('**********')
+
+# x = a
+# a = b
+# b = x
+a, b = b, a
+print('a is', a)
+print("b is", b)
+
